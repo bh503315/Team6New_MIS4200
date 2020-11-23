51a45d99f136524e7c25044dbb2f97ba032b83f5
Maggie Cho <mc488716@ohio.edu>
2020-11-23 11:44:24 -0500

    new models

---
+++ Models/Employees.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Team6New_MIS4200.Models
+{
+    public class Employees
+    {
+        [Required]
+        public Guid ID { get; set; }
+
+        [Display(Name = "Email Address Required")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter your most frequently used email address")]
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Employee First Name is required")]
+        [StringLength(20)]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Employee Last Name is required")]
+        [StringLength(20)]
+        public string lastName { get; set; }
+
+        public string fullName { get { return lastName + ", " + firstName; } }
+
+        [Display(Name = "Primary Phone")]
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$",
+            ErrorMessage = "Phone numbers must be in the format (xxx) xxx-xxxx or xxx-xxx-xxxx")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Office")]
+        [Required(ErrorMessage = "Office Name Required")]
+        [StringLength(50)]
+        public string Office { get; set; }
+
+        [Display(Name = "Current Position")]
+        [Required(ErrorMessage = "Current Position Required")]
+        [StringLength(75)]
+        public string Position { get; set; }
+
+        [Display(Name = "Hire Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
+
+        [Display(Name = "Employee Profile Picture")]
+        [Required]
+        public string photo { get; set; }
+    }
+}
---
+++ Models/Recognition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Team6New_MIS4200.Models
+{
+    public class Recognition
+    {
+        [Key]
+        public int surveyID { get; set; }
+        [Display(Name = "Core value recognized")]
+
+        public CoreValue award { get; set; }
+
+        [Display(Name = "ID of Person giving the recognition")]
+        public Guid recognizor { get; set; }
+
+        [Display(Name = "ID of Person receiving the recognition")]
+
+
+        public Guid Nominee { get; set; }
+        [Display(Name = "Date recognition given")]
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime recognizationDate { get; set; }
+
+
+        public enum CoreValue
+        {
+            Excellence = 1,
+            Integrity = 2,
+            Stewardship = 3,
+            Culture = 4,
+            Passion = 5,
+            Innovate = 6,
+            Balanced = 7
+        }
+
+    }
+}
